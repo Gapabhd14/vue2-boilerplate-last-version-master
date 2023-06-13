a49a4d2cac4f30f255464f4544a80e68b07f39a9
Gapabhd14 <gapa.bhd14@gmail.com>
2023-06-13 17:44:08 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === "production" ? "/vue2-boilerplate-last-version-master/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/tryVue/" : "/",
 
 })
